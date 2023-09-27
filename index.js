88cd5d997f7dabd8262ba7e6e98db14cb70d8766
Arslan-Zaheer <arslani1997@gmail.com>
2023-09-28 01:15:13 +0500

    In this commit, I've made various improvements to the codebase.
    feat: Implement data fetching from Firebase Realtime Database.
    
    chore: Refactoring and Data Fetching.
    
    In this commit, I've added functionality to fetch data from the Firebase Realtime Database. This will allow the application to dynamically retrieve and display information in real-time.
    
    Changes Made:
    - Added code to fetch data from the database.
    - Updated relevant functions and components to handle retrieved data.
    - Refactored function names and variable declarations for clarity.
    - Removed redundant code blocks.
    
    This enhancement contributes to a more interactive and dynamic user experience.

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import  appSettings  from './database.js'
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js"
-import { getDatabase , ref , push } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-database.js"
+import { getDatabase , ref , push , onValue } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-database.js"
 
 
 let addBtn = document.getElementById("add-button")
 let inputField = document.getElementById("input-field")
-
+let movieList = document.getElementById("movie-list")
   // Initialize DataBase
 const app = initializeApp(appSettings)
 const database = getDatabase(app)
@@ -17,6 +17,26 @@ addBtn.addEventListener("click",function(){
     push(moviesDB, inputValue)
     clearInputField()
 })
+
+onValue(moviesDB, function(snapshot){
+  let moviesArray = Object.values(snapshot.val())
+  clearUl_Elements()
+
+  for(let i = 0 ; i < moviesArray.length ; i++){
+
+    appendMovieIntoMovieList(moviesArray[i])
+
+  }
+
+})
+
+function appendMovieIntoMovieList(movie){
+    movieList.innerHTML += `<li>${movie}</li>`
+}
+
+function clearUl_Elements (){
+  movieList.innerHTML = ""
+}
 
 function clearInputField(){
   inputField.value = ""
